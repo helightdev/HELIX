b56c4c52ed4b49a1722e4a25fa7a71da669423df
HelightDev <contact@helight.dev>
2025-11-19 17:36:44 +0100

    refactor: small refactoring and cleanup

---
+++ src/HELIX/Assets/Scripts/WidgetExample.cs
@@ -0,0 +1,59 @@
+using HELIX.Widgets;
+using HELIX.Widgets.Theming;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[ThemePropertyCollection]
+public static class MyThemes {
+    public static readonly ThemeProperty<Color> PrimaryColor = ThemeProperty.Theme("c-primary", Color.white);
+
+    public static readonly ThemeProperty<Color> PrimaryWashedColor =
+        ThemeProperty.Theme("c-primary-washed", Color.white);
+
+    public static readonly ThemeProperty<WidgetFactory<VisualElement>> WidgetFactory =
+        ThemeProperty.WidgetFactory<VisualElement>("example-factory", typeof(TestFactory));
+}
+
+[UxmlWidgetFactory]
+public class TestFactory : WidgetFactory<VisualElement> {
+    public override VisualElement Create(BaseWidget parentWidget) {
+        return new Label("Hello, World!");
+    }
+}
+
+[UxmlWidgetFactory]
+public class AnotherTestFactory : WidgetFactory<VisualElement> {
+    public override VisualElement Create(BaseWidget parentWidget) {
+        return new Label("This is just another test!");
+    }
+}
+
+[UxmlElement]
+public partial class Example : BaseWidget {
+    private readonly ThemeValue<Color> _primaryColor;
+    private readonly WidgetFactorySlot<VisualElement> _factorySlot;
+
+    [UxmlAttribute]
+    public ThemeOverride<Color> PrimaryColor {
+        get => _primaryColor.Override;
+        set => _primaryColor.Override = value;
+    }
+
+    [UxmlAttribute]
+    public WidgetFactoryReference<VisualElement> FactoryReference {
+        get => _factorySlot.Reference;
+        set => _factorySlot.Reference = value;
+    }
+
+    public Example() {
+        _primaryColor = ThemeValue(MyThemes.PrimaryColor, OnPrimaryColorChanged);
+        _factorySlot = WidgetFactorySlot<VisualElement>(MyThemes.WidgetFactory);
+        _factorySlot.StretchToParentSize();
+        Add(_factorySlot);
+    }
+
+
+    private void OnPrimaryColorChanged(Color newValue) {
+        style.backgroundColor = newValue;
+    }
+}
