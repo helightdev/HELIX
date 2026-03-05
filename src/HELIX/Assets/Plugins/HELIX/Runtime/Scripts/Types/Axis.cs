f05d534d18bf424b031a7378d81cc7dda537ab5d
HelightDev <contact@helight.dev>
2026-03-05 12:13:44 +0100

    feat: Add extension methods for Axis to handle layout and size operations

--- src/HELIX/Assets/Plugins/HELIX/Runtime/Scripts/Types/Axis.cs
+++ src/HELIX/Assets/Plugins/HELIX/Runtime/Scripts/Types/Axis.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace HELIX.Types {
@@ -17,6 +18,37 @@ namespace HELIX.Types {
                 : reverse
                     ? FlexDirection.ColumnReverse
                     : FlexDirection.Column;
+        }
+
+        public static float GetLayoutSize(this Axis axis, VisualElement element) {
+            return axis == Axis.Horizontal ? element.layout.width : element.layout.height;
+        }
+
+        public static float GetRectSize(this Axis axis, Rect rect) {
+            return axis == Axis.Horizontal ? rect.width : rect.height;
+        }
+
+        public static float GetVectorComponent(this Axis axis, Vector2 vector) {
+            return axis == Axis.Horizontal ? vector.x : vector.y;
+        }
+
+        public static void SetSize(this Axis axis, VisualElement element, Length length) {
+            if (axis == Axis.Horizontal) element.style.width = length;
+            else element.style.height = length;
+        }
+
+        public static void SetBegin(this Axis axis, VisualElement element, Length length) {
+            if (axis == Axis.Horizontal) element.style.left = length;
+            else element.style.top = length;
+        }
+
+        public static void SetEnd(this Axis axis, VisualElement element, Length length) {
+            if (axis == Axis.Horizontal) element.style.right = length;
+            else element.style.bottom = length;
+        }
+
+        public static Vector2 ToCoefficient(this Axis axis) {
+            return axis == Axis.Horizontal ? Vector2.right : Vector2.up;
         }
     }
 }
