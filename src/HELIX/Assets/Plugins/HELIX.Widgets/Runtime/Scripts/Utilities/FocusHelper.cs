2efa261eae629843af4f376877d564ea64f7ab1a
Helight <helightdev@gmail.com>
2026-04-18 02:41:14 +0200

    feat: Update Focus method to include navigated parameter for improved focus handling

--- src/HELIX/Assets/Plugins/HELIX.Widgets/Runtime/Scripts/Utilities/FocusHelper.cs
+++ src/HELIX/Assets/Plugins/HELIX.Widgets/Runtime/Scripts/Utilities/FocusHelper.cs
@@ -2,8 +2,8 @@ using HELIX.Widgets.Universal.Controllers;
 
 namespace HELIX.Widgets.Utilities {
     public static class FocusHelper {
-        public static void Focus(this GlobalKey key) {
-            WidgetStateController.LastNavigated = true; // This should highlight the event once focused
+        public static void Focus(this GlobalKey key, bool navigated = false) {
+            if (navigated) WidgetStateController.LastNavigated = true; // This should highlight the event once focused
             var element = key.Target?.Element;
             element?.schedule.Execute(element.Focus).ExecuteLater(1);
         }
